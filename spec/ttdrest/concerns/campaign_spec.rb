648f260ffc6445a75a1278e14521fe96f0a1239f
Stephen Reid <stephen.reid@terminus.com>
2017-09-22 14:13:31 -0400

    Better english

--- spec/ttdrest/concerns/campaign_spec.rb
+++ spec/ttdrest/concerns/campaign_spec.rb
@@ -11,20 +11,20 @@ describe Ttdrest::Client do
       let(:start_date)  { Date.today }
 
       context "EndDate" do
-        it 'nil end date sent' do
+        it 'sets EndDate to nil when end_date is nil' do
           expect(
             client.build_campaign_data(campaign_id, name, budget, start_date, [], { end_date: nil })
           ).to include({ "EndDate" => nil })
         end
 
-        it 'date end date sent' do
+        it 'includes a formatted EndDate when a end_date is sent' do
           end_date = Date.today
           expect(
             client.build_campaign_data(campaign_id, name, budget, start_date, [], { end_date: end_date })
           ).to include({ "EndDate" => end_date.strftime("%Y-%m-%dT%H:%M:%SZ") })
         end
 
-        it 'no date does not include' do
+        it 'does not include EndDate when no date is sent' do
           expect(
             client.build_campaign_data(campaign_id, name, budget, start_date, [], { })
           ).to_not include("EndDate")
