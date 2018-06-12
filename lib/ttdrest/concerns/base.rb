9d49466d5a9a4175b931a0402aa8bb541efc10fc
Brian Malinconico <bmalinconico@terminus.com>
2018-06-12 09:36:45 -0400

    Adding instrumentation hooks for request and resonses

--- lib/ttdrest/concerns/base.rb
+++ lib/ttdrest/concerns/base.rb
@@ -1,5 +1,9 @@
+require 'date'
+require 'active_support/notifications'
+
 module Ttdrest
   class AuthorizationFailedError < StandardError; end
+  class RecoverableHttpError < StandardError; end
 
   module Concerns
     module Base
@@ -8,6 +12,7 @@ module Ttdrest
 
       VERSION = "v3"
       RETRIES = 2
+      ERROR_RETRIES = 3
 
       def authenticate(options = {})
         client_id = self.client_login || options[:client_login]
@@ -20,7 +25,59 @@ module Ttdrest
       def check_response(response)
         if response.code.eql?("403")
           raise AuthorizationFailedError
+        elsif retryable_http_error?(response)
+          raise RecoverableHttpError
         end
+      end
+
+      def retryable_http_error?(response)
+        response && (
+          response.code == 429 || (500..599).include?(response.code)
+        )
+      end
+
+      def parse_header_retry(response)
+        return nil unless response['Retry-After']
+
+        if response['Retry-After'] =~ /^\d+$/
+          sleep(response['Retry-After'].to_i)
+        else
+          date = Date.parse(response['Retry-After'])
+          wait_time = date.to_i - Time.now.to_i
+          wait_time.positive? ? wait_time : nil
+        end
+      rescue ArgumentError
+        nil
+      end
+
+      def perform_request(request, connection = http_connection)
+        retries = 0
+        response = nil
+
+        begin
+          ActiveSupport::Notifications.instrument('ttd.request') do |payload|
+            payload[:request] = request
+            payload[:retries] = retries
+
+            response = connection.request(request)
+            payload[:response] = response
+          end
+
+          check_response(response)
+        rescue RecoverableHttpError
+          raise if retries <= ERROR_RETRIES
+          retries += 1
+
+          if sleep_time = parse_header_retry
+            sleep(sleep_time)
+          else
+            sleep((1.25 ** reties) * 1 - (0.3 * rand))
+          end
+
+          retry
+        end
+
+        response
       end
 
       def get(path, params)
@@ -28,8 +85,7 @@ module Ttdrest
         begin
           request = Net::HTTP::Get.new(params.blank? ? "/#{VERSION}#{path}" : "/#{VERSION}#{path}?".concat(params.collect { |k,v| "#{k}=#{CGI::escape(v.to_s)}" }.join('&')))
           request['TTD-Auth'] = self.auth_token
-          response = http_connection.request(request)
-          check_response(response)
+          response = perform_request(request)
           result = response.body.blank? ? "" : JSON.parse(response.body)
           return result
         rescue AuthorizationFailedError
@@ -49,8 +105,7 @@ module Ttdrest
           request = Net::HTTP::Post.new("/#{VERSION}#{path}", initheader = {'Content-Type' => content_type})
           request['TTD-Auth'] = self.auth_token
           request.body = json_data
-          response = http_connection.request(request)
-          check_response(response)
+          response = perform_request(request)
           result = response.body.blank? ? "" : JSON.parse(response.body)
           return result
         rescue AuthorizationFailedError
@@ -70,8 +125,7 @@ module Ttdrest
           request = Net::HTTP::Put.new("/#{VERSION}#{path}", initheader = {'Content-Type' => content_type})
           request['TTD-Auth'] = self.auth_token
           request.body = json_data
-          response = http_connection.request(request)
-          check_response(response)
+          response = perform_request(request)
           result = response.body.blank? ? "" : JSON.parse(response.body)
           return result
         rescue AuthorizationFailedError
