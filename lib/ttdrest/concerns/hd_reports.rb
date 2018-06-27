a7b4ecd1e85d4c371e7f5a52ca1c7f730748a088
Stephen Reid <stephen.reid@terminus.com>
2018-06-27 17:29:56 -0400

    Better hd report query params

--- lib/ttdrest/concerns/hd_reports.rb
+++ lib/ttdrest/concerns/hd_reports.rb
@@ -6,10 +6,12 @@ module Ttdrest
         advertiser_id = self.advertiser_id || options[:advertiser_id]
         path = "/hdreports"
         content_type = 'application/json'
-        report_data = {
-          "AdvertiserId" => advertiser_id,
-          "ReportDateUTC" => report_date.strftime("%Y-%m-%d")
-        }
+        report_data = {}.tap do |search_params|
+          search_params["AdvertiserId"] = advertiser_id
+          search_params["ReportDateUTC"] = report_date.strftime("%Y-%m-%d")
+          search_params["ReportScheduleNameContains"] = options[:report_schedule_name_contains] if options[:report_schedule_name_contains]
+          search_params["ExecutionStates"] = options[:execution_states] if options[:execution_states]
+        end
 
         result = data_post(path, content_type, report_data.to_json)
         return result
