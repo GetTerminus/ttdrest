1974e2308d3d32080f7eb6338ace81850920775b
Stephen Reid <stephen.reid@terminus.com>
2018-06-28 10:57:07 -0400

    Add options to my reports for titles

--- lib/ttdrest/concerns/my_reports.rb
+++ lib/ttdrest/concerns/my_reports.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+# Provides API for getting MyReports
+# Documented https://api.thetradedesk.com/v3/doc/api/post-hdreports
+
 module Ttdrest
   module Concerns
     module MyReports
@@ -6,17 +11,19 @@ module Ttdrest
         advertiser_id = self.advertiser_id || options[:advertiser_id]
         path = "/myreports/reportexecution/query/advertisers"
         content_type = 'application/json'
-        report_data = {
-          "AdvertiserIds" => [advertiser_id],
-          "ExecutionSpansStartDate" => options[:execution_spans_start_date].present? ? options[:execution_spans_start_date] : report_date.strftime("%Y-%m-%dT00:00:00.0000000+00:00"),
-          "ExecutionSpansEndDate" => options[:execution_spans_end_date].present? ? options[:execution_spans_end_date] : (report_date + 1.day).strftime("%Y-%m-%dT00:00:00.0000000+00:00"),
-          "PageStartIndex" => options[:page].present? ? options[:page] : 0,
-          "PageSize" => 1,
-          "ExecutionStates" => options[:execution_states].present? ? options[:execution_states] : ["Complete"]
-        }
-        if options[:sort_fields].present?
-          report_data["SortFields"] = options[:sort_fields]
+        report_data = {}.tap do |search_params|
+          search_params["AdvertiserIds"] = [advertiser_id]
+          search_params["ReportDateUTC"] = report_date.strftime("%Y-%m-%d")
+          search_params["Type"] = options[:type] if options[:type]
+          search_params["Duration"] = options[:duration] if options[:duration]
+          search_params["ExecutionStates"] = options[:execution_states].present? ? options[:execution_states] : ["Complete"]
+          search_params["ExecutionSpansStartDate"] = options[:execution_spans_start_date].present? ? options[:execution_spans_start_date] : report_date.strftime("%Y-%m-%dT00:00:00.0000000+00:00")
+          search_params["ExecutionSpansEndDate"] = options[:execution_spans_end_date].present? ? options[:execution_spans_end_date] : (report_date + 1.day).strftime("%Y-%m-%dT00:00:00.0000000+00:00")
+          search_params["PageStartIndex"] = options[:page].present? ? options[:page] : 0
+          search_params["PageSize"] = 1
+          search_params["SortFields"] = options[:sort_fields] if options[:sort_fields]
         end
+
         result = data_post(path, content_type, report_data.to_json)
         return result
       end
