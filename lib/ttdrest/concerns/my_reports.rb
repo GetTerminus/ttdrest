116f922b29f4f0fcb25517f03b589edf3efcc60d
Stephen Reid <stephen.reid@terminus.com>
2018-07-11 10:49:09 -0400

    Add spec

--- lib/ttdrest/concerns/my_reports.rb
+++ lib/ttdrest/concerns/my_reports.rb
@@ -19,7 +19,7 @@ module Ttdrest
           search_params['ExecutionSpansStartDate'] = options[:execution_spans_start_date].present? ? options[:execution_spans_start_date] : report_date.strftime("%Y-%m-%dT00:00:00.0000000+00:00")
           search_params['ExecutionSpansEndDate'] = options[:execution_spans_end_date].present? ? options[:execution_spans_end_date] : (report_date + 1.day).strftime("%Y-%m-%dT00:00:00.0000000+00:00")
           search_params['PageStartIndex'] = options[:page].present? ? options[:page] : 0
-          search_params['PageSize'] = options[:page_size].present ? options[:page_size] : 1
+          search_params['PageSize'] = options[:page_size].present? ? options[:page_size] : 1
           search_params['SortFields'] = options[:sort_fields] if options[:sort_fields]
         end
 
